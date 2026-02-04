c3f8a5009d80683a9e93aa6bf2299e0ea9d2c155
breadbread1984 <breadbread1984@163.com>
2026-02-04 09:35:51 +0800

    update

--- prepare.sh
+++ prepare.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
 # to prevent docker image include whole public database, build the image first
-docker build -t alphafold3_cyclic -f docker/Dockerfile .
+docker build -t highfold3 -f docker/Dockerfile .
 sudo bash fetch_databases.sh /srv/shared/public_databases
 chmod 777 -R /srv/shared/public_databases
